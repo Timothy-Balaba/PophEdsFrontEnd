93615b4fffa498d31bce22df496084363a9b3c88
TIMOTHY BALABA <72457041+Timothy-Balaba@users.noreply.github.com>
2023-08-31 16:39:30 -0500

    Employee Upload tested

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './custom.scss';
 import './App.css';
-import SignIn from './SignIn';
+// import SignIn from './SignIn';
+import EmployeeProfileForm from './EmployeeProfileForm';
 // import EmployeeProfileForm from './EmployeeProfileForm';
 
 function App() {
   return (
     <div>
-    <h1 className="text-center mb-4">Samaritan Services</h1>
-    <SignIn />
+    <h1 className="text-center mb-4">Samaritan Homecare Services</h1>
+    <EmployeeProfileForm />
   </div>
   );
 }
--- src/EmployeeProfileForm.js
+++ src/EmployeeProfileForm.js
@@ -1,28 +1,56 @@
+import { useState } from "react";
+import  axios  from "axios";
 function EmployeeProfileForm(){
+
+const[firstname,setFirstName]=useState("");
+const[middlename, setMiddleName]=useState("");
+const[lastname,setLastname]=useState("");
+const[age,setAge]=useState("");
+const[email,setEmail]=useState("");
+const[phoneNumber,setPhoneNumber]=useState("");
+const[hireDate,setHireDate]=useState("");
+const[jobTitle, setJobTitle]=useState("");
+
+  const employeeProfile={
+    firstname,
+    middlename,
+    lastname,
+    age,
+    email,
+    phoneNumber,
+    hireDate,
+    jobTitle
+  }
+
+const submitting=async(e)=>{
+  e.preventDefault();
+  await axios.post(`http://localhost:8080/employeeProfile/addEmployeeProfile`,employeeProfile);
+}
+
     return(
         <div>
-        <h1>Create employee Profile</h1>
+        <h2>Create employee Profile</h2>
         <br></br>
         <body  class="grid gap-3">
         <form class="row g-3">
         <div class="row">
     <div class="col">
-      <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
+      <input type="text" class="form-control" onChange={(e)=>setFirstName(e.target.value)} placeholder="First name" aria-label="firstName"/>
     </div>
     <div class="col">
-      <input type="text" class="form-control" placeholder="Middle name" aria-label="Middle name"/>
+      <input type="text" class="form-control" onChange={(e)=>setMiddleName(e.target.value)} placeholder="Middle name" aria-label="middleName"/>
     </div>
     <div class="col">
-      <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"/>
+      <input type="text" class="form-control" onChange={(e)=>setLastname(e.target.value)} placeholder="Last name" aria-label="lastName"/>
     </div>
     <div class="col">
-      <input type="Number" class="form-control" placeholder="Age" aria-label="Age"/>
+      <input type="Number" class="form-control" onChange={(e)=>setAge(e.target.value)} placeholder="Age" aria-label="Age"/>
     </div>
 
   </div>
     <div class="col-md-6">
       <label for="inputEmail4" class="form-label">Email</label>
-      <input type="email" class="form-control" id="inputEmail4"/>
+      <input type="email" class="form-control" onChange={(e)=>setEmail(e.target.value)} id="inputEmail4"/>
     </div>
     <div class="col-md-6">
       <label for="inputPassword4" class="form-label">Password</label>
@@ -30,15 +58,15 @@ function EmployeeProfileForm(){
     </div>
     <div class="col-md-6">
       <label for="phoneNumber" class="form-label">Phone Number</label>
-      <input type="text" class="form-control" id="phoneNumber"/>
+      <input type="text" class="form-control" onChange={(e)=>setPhoneNumber(e.target.value)} id="phoneNumber"/>
     </div>
 
     <div class="col-12">
-      <input type="text" class="form-control" placeholder="Job Title" aria-label="Job Title"/>
+      <input type="text" class="form-control" onChange={(e)=>setJobTitle(e.target.value)} placeholder="Job Title" aria-label="Job Title"/>
     </div>
     <div class="col-md-6">
       <label for="Date" class="form-label">Hire Date</label>
-      <input type="date" class="form-control" id="hireDate"/>
+      <input type="date" class="form-control" onChange={(e)=>setHireDate(e.target.value)} id="hireDate"/>
     </div>
     <div class="col-12">
       <label for="inputAddress" class="form-label">Address</label>
@@ -57,22 +85,24 @@ function EmployeeProfileForm(){
       <select id="inputState" class="form-select">
         <option selected>Choose...</option>
         <option>...</option>
+        <option>Alabama</option>
+    <option>Alaska</option>
+    <optio>Arizona</optio>
+    <option>New York</option>
+    <option>New Jersey</option>
+    <option>Pennsylvania</option>
+    <option>Illinois</option>
+    <option></option>
+    <option></option>
       </select>
     </div>
     <div class="col-md-2">
       <label for="inputZip" class="form-label">Zip</label>
       <input type="text" class="form-control" id="inputZip"/>
     </div>
+   
     <div class="col-12">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck"/>
-        <label class="form-check-label" for="gridCheck">
-          Check me out
-        </label>
-      </div>
-    </div>
-    <div class="col-12">
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary" onClick={submitting}>Submit</button>
     </div>
   </form>
   </body>
